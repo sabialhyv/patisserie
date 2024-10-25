e5dbf50d58315abca16ba6ecead9942dddecbf9d
sabialhyv <sabialhyv@gmail.com>
2024-10-25 17:00:43 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import Contact from './pages/Contact'
+import Error from './pages/Error'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
+      <Navbar />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+
+      <Footer />
     </>
   )
 }
-
+ 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <>
         <nav>
-            
+            <h3>Pâtisserie aux Fraises</h3>
+            <div className="pages">
+                <Link to='/home'/>
+                <Link to='/about'/>
+                <Link to='/products'/>
+                <Link to='/contact'/>
+            </div>
         </nav>
         
         
