129cfadbb87aa7e777ddcfaf9289974f5e118158
sabialhyv <sabialhyv@gmail.com>
2024-10-25 15:03:52 +0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+        <nav>
+            
+        </nav>
+        
+        
+        </>
+    )
+}
+
+export default Navbar
