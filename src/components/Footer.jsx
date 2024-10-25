5f9637ee8b8f153f502c89d59576df612679d7dd
sabialhyv <sabialhyv@gmail.com>
2024-10-25 17:12:14 +0400

    modify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <>
+        <footer>
+            <p>2021 Patisserie. All rights reserved.</p>
+            <div className="social-media-bar">
+                <a href="#"><i className="fab fa-facebook-square"></i></a>
+                <a href="#"><i className="fab fa-instagram"></i></a>
+                <a href="#"><i className="fab fa-twitter"></i></a>
+                <a href="#"><i className="fab fa-youtube"></i></a>
+                <a href="#"><i className="fab fa-pinterest-square"></i></a>
+            </div>
+            
+        </footer>
+        
+        </>
+
+
+    )
 }
 
 export default Footer
